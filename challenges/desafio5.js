1d7fe8d9261c0a10a11955769ae57cf26d49eabd
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-08 15:19:31 -0300

    feat: new query to find products
    that has 36 likes or 85 sales, showing just the name, likes and sales.

---
+++ challenges/desafio5.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: 36 },
+      { vendidos: 85 },
+    ],
+  },
+  {
+    _id: false,
+    nome: true,
+    curtidas: true,
+    vendidos: true,
+  },
+);
