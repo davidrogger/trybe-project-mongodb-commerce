8c37d0b00f74e246a6ca85c84bcda78b9588b61d
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-08 16:31:48 -0300

    feat: new query to find products
    that has calories last than 500, showing just the name.

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
+  },
+  {
+    _id: false,
+    nome: true,
+  },
+);
