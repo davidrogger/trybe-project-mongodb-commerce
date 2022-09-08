4e51c27f0145effbab1682fd500ceabdc1f8e2a9
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-08 16:51:24 -0300

    feat: new query to update products
    add ketchup to the ingredients of all products except McChicken and avoid ingredients duplicite

---
+++ challenges/desafio12.js
@@ -0,0 +1,23 @@
+db.produtos.updateMany(
+  {
+    nome: {
+      $not: {
+        $eq: "McChicken",
+      },
+    },
+  },
+  {
+    $addToSet: {
+      ingredientes: "ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    ingredientes: true,
+  },
+);
