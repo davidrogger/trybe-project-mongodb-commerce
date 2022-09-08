9d4c281c388de83915e33ed34a12392bf4c97ad8
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-08 19:57:17 -0300

    feat: new query to update products
    field descricao to descricaoSite, and a query to check the chances showing just the name and the new field renamed

---
+++ challenges/desafio29.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {},
+  {
+    $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    descricaoSite: true,
+  },
+);
